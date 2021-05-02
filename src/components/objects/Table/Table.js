f375f5428cb90f61d9d930976b5a633f14dde7cf
Gregory Smith <gpsmith@princeton.edu>
2021-05-01 20:05:57 -0400

    spinning disc

--- src/components/objects/Table/Table.js
+++ src/components/objects/Table/Table.js
@@ -3,6 +3,19 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
 import MODEL from './Table.gltf';
 
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+    const localPrefix = isLast ? '└─' : '├─';
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+    const newPrefix = prefix + (isLast ? '  ' : '│ ');
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+      const isLast = ndx === lastNdx;
+      dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+  }
+
+let smallDisc;
 class Table extends Group {
     constructor(parent) {
         // Call parent Group() constructor
@@ -15,10 +28,11 @@ class Table extends Group {
 
         // Load object
         const loader = new GLTFLoader();
-
         this.name = 'Table';
         loader.load(MODEL, (gltf) => {
             this.add(gltf.scene);
+            console.log(dumpObject(gltf.scene).join('\n'));
+            smallDisc = gltf.scene.getObjectByName('DJControl01_1_13');
         });
 
         // Add self to parent's update list
@@ -28,6 +42,11 @@ class Table extends Group {
     update(timeStamp) {
         // Advance tween animations, if any exist
         TWEEN.update();
+        if (smallDisc) {
+            for (const disc of smallDisc.children) {
+              disc.rotation.y = timeStamp / 1000;
+            }
+          }
     }
 }
 
--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -4,6 +4,18 @@ import { Flower, Land, Spotlight } from 'objects';
 import { BasicLights } from 'lights';
 import { Speaker, Table, Truss } from '../objects';
 
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+    const localPrefix = isLast ? '└─' : '├─';
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+    const newPrefix = prefix + (isLast ? '  ' : '│ ');
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+      const isLast = ndx === lastNdx;
+      dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+  }
+
 class SeedScene extends Scene {
     constructor() {
         // Call parent Scene() constructor
@@ -29,6 +41,7 @@ class SeedScene extends Scene {
         const speaker = new Speaker(this);
         speaker.position.set(4,4,4);
         this.add(spotlight, table, lights, truss, speaker);
+        console.log(dumpObject(table).join('\n'));
 
         // Populate GUI
         this.state.gui.add(this.state, 'rotationSpeed', -5, 5);

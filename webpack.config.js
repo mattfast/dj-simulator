873bec6d3368e71cb3ae1013d55985a5083468ee
Matthew Fastow <matthewfastow@gmail.com>
2021-05-09 18:18:25 -0400

    maybe fixed deploy

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
                 exclude: path.resolve(__dirname, './node_modules/'),
             },
             {
-                test: /\.(jpe?g|png|gif|svg|tga|gltf|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg)$/i,
+                test: /\.(jpe?g|png|gif|svg|tga|gltf|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg|bin)$/i,
                 use: 'file-loader',
                 exclude: path.resolve(__dirname, './node_modules/'),
             },
@@ -49,6 +49,8 @@ module.exports = {
             lights$: path.resolve(__dirname, 'src/components/lights'),
             objects$: path.resolve(__dirname, 'src/components/objects'),
             scenes$: path.resolve(__dirname, 'src/components/scenes'),
+            audio$: path.resolve(__dirname, 'src/components/audio'),
+            textures$: path.resolve(__dirname, 'src/components/textures'),
         },
     },
     plugins: [
